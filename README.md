cf7bc59e2bb91d4e5ca127420ef5d4d4b4c17e39
= <harrison.lindgren@gmail.com>
2020-09-20 22:24:18 -0400

    Final commit for the night; finished up most of the work

--- README.md
+++ README.md
@@ -1,5 +1,25 @@
-## Installation
+# ocr-word-censor
+
+### A small script that I made to do the screenshot censoring work for me.
+
+I found that sometimes I would miss names when censoring screenshots, so I figured that it would be useful to have a computer to the double checking for me. This program will go through the screenshot that you give it and censors any matches that are found.
+
+## Warnings:
+
+- This program seems to work so-so on computer screenshots, due to their low quality and very antialiased state. High quality phone screenshots are what the program best works on. It was tested using my Pixel 4A and there were no errors encountered, however on my PC it only worked about 2/3 times.
+
+- This program is not a substitute for actually scanning sensitive documents. If censoring really matters, then do it yourself, or using a better tool.
+
+## Installation:
 
 `sudo apt install tesseract-ocr python3 python3-pip`
 
 `pip3 install -r requirements.txt`
+
+## Usage:
+
+`python3 ./censor.py -i [INPUT] -o [OUTPUT] -w [WORD] -c [COLOR]`
+
+Example (censors all "idiot"s in the screenshot):
+
+`python3 ./censor.py -i ./SCREENSHOT_1234567890.png -o output.png -w "Idiot" -c "#000000"`
--- censor.py
+++ censor.py
@@ -5,6 +5,7 @@ parser = argparse.ArgumentParser(description = 'Censors the words on an image')
 parser.add_argument('--input-file', '-i', type = str, help = 'Input file path', required = True)
 parser.add_argument('--output-file', '-o', type = str, help = 'Output file path', required = True)
 parser.add_argument('--word', '-w', type = str, help = 'Word to censor', required = True)
+parser.add_argument('--color', '-c', type = str, help = 'Hex color to censor text with', required = True)
 
 # parse the command line args
 arguments = parser.parse_args()
@@ -23,6 +24,7 @@ image = imageEnhancer.enhance(2) # the optimal value seems to be 2; I have no id
 # create an object that can be used to draw on the image
 imageDraw = PIL.ImageDraw.Draw(image)
 
+'''
 # convert the image to boxes that can be used to erase words
 imageCharacterData = pytesseract.image_to_boxes(image, lang = 'eng')
 
@@ -37,5 +39,25 @@ for char in imageCharacterData.splitlines():
     ]
     text = charData[0]
     imageDraw.rectangle(coordinates, outline = '#00ff00')
+'''
+
+# convert the image to word data
+imageData = pytesseract.image_to_data(image, lang = 'eng', output_type = pytesseract.Output.DICT)
+
+# iterate through the words by their integer index and not their value
+for index in range(len(imageData['text'])):
+    if (imageData['text'][index].lower() == arguments.word.lower()):
+
+        dimensions = [imageData['width'][index], imageData['height'][index]]
+        position = [imageData['left'][index], imageData['top'][index]]
+
+        print('MATCH FOUND AT X:{} Y:{}'.format(position[0], position[1]))
+
+        imageDraw.rectangle([
+            position[0],
+            position[1],
+            position[0] + dimensions[0],
+            position[1] + dimensions[1]
+        ], fill = arguments.color)
 
 image.save(arguments.output_file)
